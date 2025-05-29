5b150611e4bb4c74e3cf717243edb80e218197e5
Jörg Richter <jri@deepamehta.de>
2025-05-29 14:35:58 +0200

    Add build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+js="main.js"
+min="main.min.js"
+html="elm-timelines.html"
+
+elm make src/Main.elm --optimize --output=$js
+
+uglifyjs $js --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | uglifyjs --mangle --output $min
+
+echo "Initial size: $(cat $js | wc -c) bytes ($js)"
+echo "Minified size:$(cat $min | wc -c) bytes ($min)"
+
+./insert-script.js
+
+echo "Standalone:   $(cat $html | wc -c) bytes ($html) (gzipped: $(cat $html | gzip -c | wc -c) bytes)\n"
